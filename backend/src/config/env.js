7b7c11e2866e8e9a63ed0b6db53abfee364760f7
Duc Anh Dang <danganhbill0806@gmail.com>
2026-02-25 16:52:17 -0600

    setup: firebase connections

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const required = [
   'PORT',
   'SUPABASE_URL',
-  'SUPABASE_SERVICE_KEY'
+  'SUPABASE_SERVICE_KEY',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_PRIVATE_KEY',
+  'FIREBASE_CLIENT_EMAIL',
 ];
 
 required.forEach((key) => {
@@ -20,4 +23,7 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   SUPABASE_URL: process.env.SUPABASE_URL,
   SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
 };
