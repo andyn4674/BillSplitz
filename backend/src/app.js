506b91b17f5515d5e4fa758cf00cb38aab71f08c
Duc Anh Dang <danganhbill0806@gmail.com>
2026-02-25 19:54:07 -0600

    chores: add some comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+// global middleware runs on every request
+// without this: the req.body will be undefined, express does not do it automatically
 app.use(express.json());
 
 app.use('/auth', require('./modules/auth/auth.routes.js'));
@@ -14,7 +16,6 @@ app.get('/health', async (req, res) => {
 });
 
 // Global error handler
-
 app.use(require('./middleware/errorHandler.js'));
 
 module.exports = app;
